70dcc0cb3226f7ae28ae3c932a492655c43e2f92
Roy Ratcliffe <roy@ratcliffe.me>
2019-01-13 15:01:52 +0000

    Implemented vec_scale/3

--- prolog/clp/vec.pl
+++ prolog/clp/vec.pl
@@ -1,12 +1,25 @@
 :- module(clp_vec,
-          [   vec_translate/3           % ?U, ?V, ?W
+          [   vec_translate/3,          % ?U, ?V, ?W
+              vec_scale/3               % ?Scalar, ?U, ?V
           ]).
 
 :- use_module(library(clpr)).
 
 %!  vec_translate(?U, ?V, ?W) is nondet.
+%
+%   Vector addition.
 
 vec_translate([], [], []).
 vec_translate([H0|T0], [H1|T1], [H|T]) :-
     {H =:= H0 + H1},
     vec_translate(T0, T1, T).
+
+%!  vec_scale(?Scalar, ?U, ?V) is nondet.
+%
+%   Scalar multiplication. Scaling  by  a   negative  scalar  flips  the
+%   direction, as well as scales by the magnitude.
+
+vec_scale(_, [], []).
+vec_scale(Scalar, [H0|T0], [H|T]) :-
+    {H =:= Scalar * H0},
+    vec_scale(Scalar, T0, T).
--- prolog/clp/vec.plt
+++ prolog/clp/vec.plt
@@ -14,4 +14,11 @@ test(vec_translate, [true(V == [2.0, 2.0]), nondet]) :-
 test(vec_translate, [true(U == [1.0, 1.0]), nondet]) :-
     vec_translate(U, [2, 2], [3, 3]).
 
+test(vec_scale, [true(V == [10.0, 10.0, 10.0]), nondet]) :-
+    vec_scale(10, [1, 1, 1], V).
+test(vec_scale, [true(U == [1.0, 1.0, 1.0]), nondet]) :-
+    vec_scale(10, U, [10, 10, 10]).
+test(vec_scale, [true(Scalar == 10.0), nondet]) :-
+    vec_scale(Scalar, [1, 1, 1], [10, 10, 10]).
+
 :- end_tests(clp_vec).
